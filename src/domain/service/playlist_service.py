5c1c3e87db77810a5beeb346da2ac8cfe0802038
Higor F. Oliveira <higoroliveiiira@gmail.com>
2020-06-24 22:51:38 -0300

    Remove credentials

--- src/domain/service/playlist_service.py
+++ src/domain/service/playlist_service.py
@@ -6,9 +6,9 @@ from src.domain.service.classic_service import ClassicService
 
 
 class PlaylistService:
-    API_WEATHER_KEY = "47ab211f0f5a3f89151850edee950e3f"
-    API_WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5"
-    API_KEY_SPOTIFY = "BQC-pDD25Gf9SaU5-Viyim3HWzEz1Hd-6Co1r0jsV7AtFgmrsxaklA4-u5TG0qoODWoEqEgvT_SvLpx-WbXdIYWGzHCS_8D-asvzLncmc6cRbVDvxcCGzWZkZmjnTJOmrKLILWUuA5pP2_iE77dV3vtXJfjkjxI_KajSZ6y_aRQmuM_1-PzaWuK8UMJH4bsdEaZCavkSPeBWy78t_lcoVQHa-qnUD_g3cioY9sbgEMYOvjmfYjD2n5LQ1QUkvjHkYb12Hlu7cV-JXiwsUPEVK2YjdXPeRIBT"
+    API_WEATHER_KEY = "my-fake-token"
+    API_WEATHER_BASE_URL = "my-fake-token"
+    API_KEY_SPOTIFY = "my-fake-token"
 
     def __init__(self, http_client: HttpClient):
         self._http_client = http_client
