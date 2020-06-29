a223a69994c207e43ad9b26749b415ce89fb8401
Higor F. Oliveira <higoroliveiiira@gmail.com>
2020-06-29 19:39:10 -0300

    Changing database connection

--- app/config/database.py
+++ app/config/database.py
@@ -4,12 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{}:{}@{}/{}".format(environ['user'],
-                                                               environ['pass'],
-                                                               environ['host'],
-                                                               environ['db_name'])
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{}:{}@{}:{}/{}".format(environ['user'],
+                                                                 environ['pass'],
+                                                                 environ['host'],
+                                                                 environ['port'],
+                                                                 environ['db_name'])
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=28800 - 1, pool_pre_ping=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
