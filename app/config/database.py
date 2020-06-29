8c51b3f322cd12126cb24b9421acb4b9a50d32fd
Higor F. Oliveira <higoroliveiiira@gmail.com>
2020-06-29 20:39:09 -0300

    Change to postgres

--- app/config/database.py
+++ app/config/database.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{}:{}@{}:{}/{}".format(environ['user'],
-                                                                 environ['pass'],
-                                                                 environ['host'],
-                                                                 environ['port'],
-                                                                 environ['db_name'])
+SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}:{}/{}".format(environ['user'],
+                                                               environ['pass'],
+                                                               environ['host'],
+                                                               environ['port'],
+                                                               environ['db_name'])
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
